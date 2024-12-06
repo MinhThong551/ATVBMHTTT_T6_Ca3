7b282722c89790ded40ddaa940aece14418a48f4
mlnhquxc <quangminhnguyn26@gmail.com>
2024-12-06 14:44:49 +0700

    Add form manager key pair(input key, report, save key, load key from File)

---
+++ WebBanBanh/src/main/java/nhom55/hcmuaf/controller/user/UpdateKeyPairUser.java
@@ -0,0 +1,37 @@
+package nhom55.hcmuaf.controller.user;
+
+import nhom55.hcmuaf.beans.Users;
+import nhom55.hcmuaf.enums.LogLevels;
+import nhom55.hcmuaf.log.AbsDAO;
+import nhom55.hcmuaf.log.Log;
+import nhom55.hcmuaf.log.RequestInfo;
+import nhom55.hcmuaf.services.UserService;
+import nhom55.hcmuaf.util.MyUtils;
+import nhom55.hcmuaf.util.UserValidator;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@WebServlet(name = "updateKeyPairUser", value = "/page/user/update-key-pair")
+public class UpdateKeyPairUser extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = this.getServletContext()
+                .getRequestDispatcher("/WEB-INF/user/update-key-pair.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Users user = MyUtils.getLoginedUser(session);
+
+    }
+}
